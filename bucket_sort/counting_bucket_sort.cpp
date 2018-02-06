c44176233d7a75e20c51683c4f081ddf9cb9cb87
Xie Xie <DSAD2015@163.com>
2018-02-06 16:49:39 +0800

    Add files via upload

---
+++ bucket_sort/counting_bucket_sort.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <random>
+#include <ctime>
+
+int main()
+{
+  double start_t, end_t;  // 计时器.
+
+  // 内存分配计时开始.
+  start_t = clock();
+  std::vector<double> V(500000000);  // 5亿个数.
+  // 内存分配计时结束并输出时间.
+  end_t = clock();
+  std::cout << (end_t - start_t) / (CLOCKS_PER_SEC * 60) << " minutes" << std::endl;
+
+  // 数据赋值计时开始.
+  start_t = clock();
+  // 利用随机数生成器生成0.0到1.0之间的实数.
+  std::default_random_engine generator(time(NULL));
+  std::uniform_real_distribution<double> distribution(0.0, 1.0);
+  for (size_t i = 0; i < V.size(); ++i)
+    V[i] = distribution(generator);
+  // 数据赋值计时结束并输出时间.
+  end_t = clock();
+  std::cout << (end_t - start_t) / (CLOCKS_PER_SEC * 60) << " minutes" << std::endl;
+
+  // 排序计时开始.
+  start_t = clock();
+  size_t E = 20;  // 每个桶中期望元素个数.
+  std::vector<size_t> C(V.size() / E + 1, 0);     // 使用计数方法.
+  std::vector<double> B(V.size());                // 另一种形式的桶.
+  // 对每个桶的元素进行计数.
+  for (const auto& x : V)
+    ++C[x * (V.size() / E) + 1];
+  std::vector<size_t> P(C); // 位置指示器.
+  // 将V中元素分配到不同的桶中.
+  for (const auto& x : V)
+    B[P[x * (V.size() / E)]++] = x;
+  // 对不同的桶进行排序, 区间为[left, right).
+  auto left = B.begin();
+  for (size_t i = 1; i < C.size(); ++i)
+  {
+    auto right = left + C[i];
+    std::sort(left, right);
+    left = right;
+  }
+  // 利用交换将数据放回原向量中.
+  V.swap(B);
+  // 排序计时结束并输出时间.
+  end_t = clock();
+  std::cout << (end_t - start_t) / (CLOCKS_PER_SEC * 60) << " minutes" << std::endl;
+
+  int a;
+  std::cin >> a;
+
+  return 0;
+}
