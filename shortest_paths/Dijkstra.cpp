d5b901da3103c9db6a1aadc5c8e602ce1e2634c3
Xie Xie <DSAD2015@163.com>
2018-12-12 02:54:25 +0800

    Dijkstra

--- shortest_paths/Dijkstra.cpp
+++ shortest_paths/Dijkstra.cpp
@@ -6,9 +6,22 @@ using namespace std;
 
 const double POSITIVE_INFINITY = numeric_limits<double>::max();
 
+// 按照直观习惯, 次序是先写顶点后写权重. 当然直接用pair也可以, 但不够清晰.
 struct neighbor_information {
-  size_t v;
+  size_t vertex;
   double weight;
+};
+
+// 必须定义比较准则才能使用neighbor_information集合.
+struct cmp_neighbor_weight {
+	bool operator()(const neighbor_information& a,
+                  const neighbor_information& b) const
+	{
+    // 利用pair的字典序, 这样会少写很多判断, 这里以weight为主, 注意pair定义略有不同.
+    using P = pair<double, size_t>;
+    // 难点在于先要对weight分大于、小于和等于三种情况，等于情况下还得继续处理vertex.
+    return P(a.weight, a.vertex) < P(b.weight, b.vertex);
+	}
 };
 
 void Dijkstra(vector<vector<neighbor_information>>& WG, size_t s,
@@ -28,32 +41,32 @@ void Dijkstra(vector<vector<neighbor_information>>& WG, size_t s,
   // s位置路径长度初值为0, 这样还可让s最先被找到.
   length[s] = 0;
   // 尚未寻找到的顶点集合为NS.
-  set<pair<double, size_t>> NS;
+  set<neighbor_information, cmp_neighbor_weight> NS;
   // 顶点在集合NS中的位置存于vertex_iter中.
-  vector<set<pair<double, size_t>>::iterator> vertex_position(V);
+  vector<set<neighbor_information>::iterator> vertex_position(V);
   for (size_t u = 0; u < V; u++)
-    vertex_position[u] = NS.insert({length[u], u}).first;
+    vertex_position[u] = NS.insert({u, length[u]}).first;
   for (size_t i = 0; i < V; i++)
   {
     // 每次在未找到的顶点集合中, 寻找以s为起点的路径长度最短的顶点.
     // 设本次所找到的顶点为u, 对应路径长度为min.
-    double min = NS.begin()->first;
-    size_t u = NS.begin()->second;
+    double min = NS.begin()->weight;
+    size_t u = NS.begin()->vertex;
     // u已找到, 将其从NS中删除.
     NS.erase(NS.begin());
     // 以u的邻接表为依据更新相关数据.
     for (const auto& neighbor : WG[u])
-      if (length[u] + neighbor.weight < length[neighbor.v])
+      if (length[u] + neighbor.weight < length[neighbor.vertex])
       {
         // 为方便表述, 将当前所处理顶点为neighbor.v简记为v.
-        size_t v = neighbor.v;
+        size_t v = neighbor.vertex;
         // 若lenght[u] + w(u, v) < length[v],
         // 则更新path[v]和length[v].
         path[v] = u;
         length[v] = length[u] + neighbor.weight;
         // 通过删除和重新插入改变NS中的相应取值并保存位置.
         NS.erase(vertex_position[v]);
-        vertex_position[v] = NS.insert({length[v], v}).first;
+        vertex_position[v] = NS.insert({v, length[v]}).first;
       }
     }
 }
