78493ad6727a26b10e1d27c2c5d76e9f4a2b6c73
Xie Xie <DSAD2015@163.com>
2019-09-01 19:05:41 +0800

    Update maze.cpp

--- backtrack/maze.cpp
+++ backtrack/maze.cpp
@@ -16,9 +16,9 @@ const char visited = 'V';   // 访问过的标记.
 // 迷宫字符数组, 周围一圈全是墙(以'*'标记).
 char maze[m][n] = {
     {'*', '*', '*', '*', '*', '*', '*'},
-    {'*', '0', '*', '0', '0', '0', '*'},
-    {'*', '0', '*', '0', '*', '0', '*'},
     {'*', '0', '0', '0', '*', '0', '*'},
+    {'*', '0', '*', '*', '*', '0', '*'},
+    {'*', '0', '0', '0', '0', '0', '*'},
     {'*', '*', '*', '*', '*', '*', '*'}
 };
 const int d = 4;      // 可行方向总数.
@@ -30,25 +30,26 @@ point destination = {3, 5};
 
 // 递归形式求解迷宫, 实际上不用递归更好, 避免了参数传递, 但要保存每个点的direction当前值.
 bool solved = false;
-void backtrack(vector<point>& X, size_t k)
+void backtrack(vector<point>& X)
 {
-  if (X[k - 1].x == destination.x && X[k - 1].y == destination.y)
+  if (X.back().x == destination.x && X.back().y == destination.y)
   {
     solved = true;
-    for (size_t i = 0; i < k; ++i)
-      cout << X[i].x << ' ' << X[i].y << endl;
+    for (const auto& c : X)
+      cout << c.x << ' ' << c.y << endl;
   }
   else
   {
     for (int direction = 0; direction < d; ++direction)
     {
-      point next = {X[k - 1].x + delta[direction].x,
-                    X[k - 1].y + delta[direction].y};
+      point next = {X.back().x + delta[direction].x,
+                    X.back().y + delta[direction].y};
       if (maze[next.x][next.y] == unvisited)
       {
         maze[next.x][next.y] = visited;
-        X[k] = next;
-        backtrack(X, k + 1);
+        X.push_back(next);
+        backtrack(X);
+        X.pop_back();
         if (solved)
           return;
       }
@@ -59,9 +60,9 @@ void backtrack(vector<point>& X, size_t k)
 int main()
 {
   vector<point> P;
-  P.resize(m * n);
-  P[0] = source;
+  P.reserve(m * n);
+  P.push_back(source);                      // 初始点设为入口点.
   maze[source.x][source.y] = visited;
-  backtrack(P, 1);
+  backtrack(P);
   return 0;
 }
