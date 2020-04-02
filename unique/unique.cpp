c76157c00a05a531be8cd67f19f3bde449b6619d
Xie Xie <DSAD2015@163.com>
2020-04-02 17:00:23 +0800

    Update unique.cpp

--- unique/unique.cpp
+++ unique/unique.cpp
@@ -1,43 +1,96 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <set>
+#include <unordered_set>
 
-void unique_old(std::vector<int>& S)
+using namespace std;
+
+void simple_count(vector<int>& V)
 {
-  sort(S.begin(), S.end());
-  for (size_t L = 0, R; L < S.size(); L = R)
+  sort(V.begin(), V.end());
+  for (size_t left = 0, right; left < V.size(); left = right)
   {
-    for (R = L + 1; R < S.size(); ++R)
-      if (S[R] != S[L])
+    for (right = left + 1; right < V.size(); ++right)
+      if (V[right] != V[left])
         break;
-    std::cout << S[L] << ": " << R - L << std::endl;
+    cout << V[left] << ": " << right - left << endl;
   }
 }
 
-void unique(std::vector<int>& S)
+void count(vector<int>& V)
 {
-  sort(S.begin(), S.end());
-  size_t L, R;
-  for (L = 0, R = 0; R < S.size(); ++R)
-    if (S[R] != S[L])
+  sort(V.begin(), V.end());
+  size_t left, right;
+  for (left = 0, right = 1; right < V.size(); ++right)
+    if (V[right] != V[left])
     {
-      std::cout << S[L] << ": " << R - L << std::endl;
-      L = R;
+      cout << V[left] << ": " << right - left << endl;
+      left = right;
     }
-  if (S.size() > 0)
-    std::cout << S[L] << ": " << R - L << std::endl;
+  if (V.size() > 0)
+    cout << V[left] << ": " << right - left << endl;
+}
+
+void unique(vector<int>& V)
+{
+  // 因为必须判断V是否为空, 所以提前做可以节约时间.
+  if (V.size() == 0)
+    return;
+  sort(V.begin(), V.end());
+  size_t left = 0;
+  for (size_t right = left + 1; right < V.size(); ++right)
+    if (V[right] != V[left])
+      V[++left] = V[right];
+  // 注意空向量情况不加判断直接执行下面这句会出错.
+  V.resize(++left);
+}
+
+template <typename T, typename U>
+void unique_with_set(vector<T>& V, U& S)
+{
+  S.clear();
+  for (const auto& x : V)
+    S.insert(x);
+  V.resize(S.size());
+  copy(S.begin(), S.end(), V.begin());
+  S.clear();
+}
+
+template <typename T>
+void print_all(const T& S)
+{
+  for (const auto& x : S)
+    cout << x << ' ';
+  cout << endl;
 }
 
 int main()
 {
-  std::cout << "Example A: " << std::endl;
-  std::vector<int> A = {7, 2, 3, 2, 5, 1, 1};
+  cout << "Example A: " << endl;
+  vector<int> A = {7, 2, 3, 2, 5, 1, 1};
+  count(A);
   unique(A);
-  std::cout << "Example B: " << std::endl;
-  std::vector<int> B = {-9};
+  print_all(A);
+  cout << "Example B: " << endl;
+  vector<int> B = {-9};
+  count(B);
   unique(B);
-  std::cout << "Example C: " << std::endl;
-  std::vector<int> C;
+  print_all(B);
+  cout << "Example C: " << endl;
+  vector<int> C;
+  count(C);
   unique(C);
+  print_all(C);
+  cout << "Example D: " << endl;
+  vector<int> D = {1, 2, 1, 2, 1, 1, 1};
+  vector<int> E = D;
+  set<int> F;
+  unordered_set<int> G;
+  unique_with_set(D, F);
+  print_all(D);
+  cout << "Example E: " << endl;
+  unique_with_set(E, G);
+  print_all(E);
   return 0;
 }
